39c06f160ba7017332fef6d7142794be974981b5
findById <328559929@qq.com>
2017-01-09 18:09:08 +0800

    Initial commit

--- server/server.go
+++ server/server.go
@@ -65,7 +65,7 @@ func handleList(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	br := bufio.NewReader(strings.NewReader(string(buffer)))
-	array := make([]map[string]string,0)
+	array := make([]MediaItem, 0)
 	for {
 		temp, _, err := br.ReadLine()
 		if err != nil {
@@ -77,14 +77,19 @@ func handleList(w http.ResponseWriter, r *http.Request) {
 			continue
 		}
 
-		model := make(map[string]string)
 		index := strings.Index(line, ":")
 		if index > 0 && index < len(line) {
 			key := strings.TrimSpace(line[0:index])
 			value := strings.TrimSpace(line[index + 1:])
-			model[key] = value
+			log.Println(key, value)
+			item := MediaItem{}
+			err := json.Unmarshal([]byte(value), &item)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
 
-			array = append(array, model)
+			array = append(array, item)
 		}
 	}
 
@@ -113,13 +118,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if isExists(*targetDir + "/" + key + "/success") {
 		log.Println("already exists")
 		w.Write([]byte(key))
-		return;
+		// return;
 	}
+	index := strings.LastIndex(source, "/")
+	dot := strings.LastIndex(source, ".")
+	filename := ""
+	if index > 0 && index < len(source) {
+		filename = source[index:dot]
+	} else {
+		filename = source[:dot]
+	}
+	hlsPath := "/" + key + "/" + filename + ".m3u8"
 
-	status := execute(fmt.Sprintf(hls, *sourceDir + "/" + source, *targetDir + "/" + key + "/playlist.m3u8"))
+	status := execute(fmt.Sprintf(hls, *sourceDir + "/" + source, *targetDir + hlsPath))
 	if status == 1 {
 		ioutil.WriteFile(*targetDir + "/" + key + "/success", []byte("success"), 0666)
-		appendList(source, key + "/playlist.m3u8")
+
+		item := new(MediaItem)
+		item.Id = key
+		item.SourcePath = source
+		item.HlsPath = hlsPath
+		item.Filename = filename
+
+		appendList(source, item)
 	}
 
 	w.Header().Add("content-type", "text/html;charset=utf-8")
@@ -135,12 +156,28 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write(b)
 }
 
-func appendList(key, value string) {
-	log.Println(key, value)
-	f, _ := os.OpenFile("list", os.O_APPEND | os.O_WRONLY, 0666)
+type MediaItem struct {
+	Id         string        `json:"id"`
+	Filename   string        `json:"filename"`
+	SourcePath string        `json:"sourcePath"`
+	HlsPath    string        `json:"hlsPath"`
+}
+
+func appendList(key string, item *MediaItem) {
+	b, err := json.Marshal(item)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(key, string(b))
+	f, err := os.OpenFile("list", os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0666)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	defer f.Close()
 
-	f.WriteString(key + " : " + value + "\r\n")
+	f.WriteString(item.Id + " : " + string(b) + "\r\n")
 }
 
 func isExists(path string) bool {
